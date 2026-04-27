216e22120168c6dd6697392cb26b23c49f7c269b
AngelPC0304 <juan.paredesc@alumno.buap.mx>
2026-04-27 17:30:13 -0600

    trabajando en localstorage
    Cambiamos el serials.py y jalamos el nombre del usuario para mostrar el dashboard

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
 from .models import Categoria, Producto
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer # <- 1. NUEVA IMPORTACIÓN
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,7 +18,8 @@ class RegistroSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'first_name', 'rol'] 
+        # 2. Agregamos 'last_name' a la lista de campos permitidos
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'rol'] 
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -27,7 +29,8 @@ class RegistroSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
-            first_name=validated_data.get('first_name', '')
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '') # <- 3. LO GUARDAMOS EN LA BASE DE DATOS
         )
 
         # aqui asigna el rol
@@ -35,3 +38,14 @@ class RegistroSerializer(serializers.ModelSerializer):
         user.groups.add(grupo)
 
         return user
+
+# 4. Ahora si les tiene que mostrar el nombre
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        
+        data = super().validate(attrs)
+        
+        data['nombre'] = self.user.first_name
+        data['apellidos'] = self.user.last_name
+        
+        return data
